55ee8a97cc50d4827b442af04edf098cd034779e
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-09-15 22:39:55 +0530

    created nodes

---
+++ MyBinaryTreeTest.java
@@ -0,0 +1,12 @@
+
+public class MyBinaryTreeTest {
+
+	public static void main(String[] args) {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		System.out.println("Size of the tree is "+myBinaryTree.getSize());
+	}
+
+}
